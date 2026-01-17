6449f07fe02975660f6a269a617f10ae22027da8
Jérôme Welscher <welscherjerome@gmail.com>
2026-01-17 16:47:29 +0100

    delete unnecessary files

--- MCMC_bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-c_values=(0.05 0.1 0.2 0.3 0.4 0.5)
-
-for c in "${c_values[@]}"; do
-    echo "Running MCMC simulation: $c"      
-    sudo python main.py -N 5 -t 50000 -i 3 -f "MCMC" -c "$c" -m MCMC -p 0 0
-done
-
--- reset_locks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Use shell script in very rare case of lingering Ained locks
-
-echo "Forcing removal of all Ained locks..."
-rm /dev/shm/sem_sem_* 2>/dev/null
-echo "Done. You can run your program now."
